211328690e98039d3fea382608018183a465388c
andrewstech <andrew@win11react.com>
2023-08-06 21:13:13 +0000

    smtp update

--- smtp.js
+++ smtp.js
@@ -1,6 +1,16 @@
 const fs = require("fs");
 const SMTPServer = require("smtp-server").SMTPServer;
+const nodemailer = require('nodemailer');
 const smtpPort = 25;
+
+let transporter = nodemailer.createTransport({
+  service: 'postfix',
+  host: 'localhost',
+  secure: false,
+  port: 2525,
+  //auth: { user: 'yourlinuxusername@edison.example.com', pass: 'yourlinuxuserpassword' },
+  tls: { rejectUnauthorized: false }
+});
 
 const server = new SMTPServer({
   secure: false, // Set to true for secure connections (TLS)
@@ -38,8 +48,30 @@ const server = new SMTPServer({
       data += chunk;
     });
     stream.on("end", () => {
-      console.log("Received email data:", data);
-      callback();
+      transporter.on("end", () => {
+        console.log("Email sent successfully!");
+        callback();
+      });
+    
+      transporter.on("error", (err) => {
+        console.error("Error sending email:", err);
+        callback();
+      });
+    
+      // Send email data
+      transporter.send(
+        {
+          from: mailFrom,
+          to: rcptTo,
+          data: data,
+        },
+        (err) => {
+          if (err) {
+            console.error("Error sending email:", err);
+          }
+          transporter.quit();
+        }
+      );
     });
   },
   onMailFrom(address, session, callback) {
