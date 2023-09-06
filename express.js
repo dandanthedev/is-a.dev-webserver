390a00ed332257c74196ed7b59197814a1860178
andrewstech <andrew@win11react.com>
2023-09-06 21:26:21 +0000

    fix

--- express.js
+++ express.js
@@ -176,6 +176,25 @@ app.get('/api/download', async (req, res) => {
   });
 });
 
+app.get("/api/SMTP", async (req, res) => {
+  let domain = req.query.domain;
+  let jwt = req.query.jwt;
+  let enabled = req.query.enabled;
+  let user = getJWT(jwt);
+  if (!user) return res.status(403).send("Invalid JWT");
+  if (!domain) return res.status(400).send("No domain provided");
+  let data = await fetch(process.env.API_URL + "/domains/" + domain + "/get");
+  data = await data.json();
+  if (data.error) return res.status(500).send(data.error);
+  if (data.owner?.username != user.user.login)
+    return res
+      .status(403)
+      .json({ error: "You are not the owner of this domain" });
+  await User.findOneAndUpdate(
+    { domain },
+    { EMAIL: enabled })
+  return res.json({ success: true });
+
 app.get("/api/pannel", async (req, res) => {
   let jwt = req.query.jwt;
   let domain = req.query.domain;
