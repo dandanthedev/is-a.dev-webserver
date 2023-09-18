432b8b3d957f259f060dfa0d9f2f57a12c39727b
andrewstech <andrew@win11react.com>
2023-09-18 19:01:05 +0000

    Bug Fix

--- express.js
+++ express.js
@@ -401,7 +401,7 @@ app.get("/api/register", async (req, res) => {
     data = await data.json();
 
     if (data.error) return res.status(500).send(data.error);
-    if (data.owner?.username != user.user.login)
+    if (data.owner?.username.toLowerCase() != user.user.login.toLowerCase())
       return res
         .status(403)
         .json({ error: "You are not the owner of this domain" });
